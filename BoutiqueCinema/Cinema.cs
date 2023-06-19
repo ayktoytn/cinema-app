c94c2636bc3ef4828321bae1b3354a6caa4333df
ayktoytn <38761448+ayktoytn@users.noreply.github.com>
2023-06-20 00:11:27 +0300

    Add files via upload
    With this application, it will be easier to manage a movie theater. For a movie you enter the name of, you can determine the capacity of the theater and the price of the movie, sell tickets or refund tickets. As a result of these, you can also see the income you have earned and the current status of the hall.
    ---------------------------------
    Bu uygulama ile bir sinema salonunu yönetmek daha kolay olacaktır. Adını girdiğiniz bir film için salonun kapasitesini ve filmin fiyatını belirleyebilir, bilet satışı veya bilet iadesi yapabilirsiniz. Bunların sonucunda elde ettiğiniz geliri ve salonun güncel durumunu da görebilirsiniz.

---
+++ BoutiqueCinema/Cinema.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoutiqueCinema
+{
+    internal class Cinema
+    {
+       // Properties 
+        public string MovieName;
+        public int Capacity { get; }
+        public float FullTicketPrice { get; set; }
+        public float HalfTicketPrice { get; set; }
+        public int SumFullTicket { get; set; }
+        public int SumHalfTicket { get; set; }
+        public float Turnover
+        {
+            get 
+            {
+                return this.FullTicketPrice * this.SumFullTicket + this.HalfTicketPrice * this.SumHalfTicket;
+            }
+           
+        }
+
+        //constructor with parameters
+
+
+        // defining the object parameters
+        public Cinema(string movie, int cap, float full, float half)
+        {
+            this.MovieName = movie;
+            this.Capacity = cap;
+            this.HalfTicketPrice = half;
+            this.FullTicketPrice = full;
+        }
+        public void SellingTicket(int full, int half)
+        {
+            if (GetEmptySeat() >= full + half)
+            {
+                this.SumFullTicket += full;
+                this.SumHalfTicket += half;
+
+                
+                Console.WriteLine("Purchase complete.");
+            }
+            else
+            {
+                Console.WriteLine("There are not enough empty seats. Number of the available seats is: " + GetEmptySeat());
+            }
+        }
+        public void RefundTicket(int full, int half)
+        {
+            if (this.SumFullTicket >= full && this.SumHalfTicket >= half)
+            {
+                this.SumFullTicket -= full;
+                this.SumHalfTicket -= half;
+                
+                Console.WriteLine("Ticket has been refunded.");
+            }
+            else
+            {
+                Console.WriteLine("You have exceeded the amount of refundable tickets.");
+            }
+        }
+       
+        public int GetEmptySeat()
+        {
+            return this.Capacity - this.SumHalfTicket - this.SumFullTicket;
+        }
+    }
+}
---
+++ BoutiqueCinema/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace BoutiqueCinema
+{
+    internal class Program
+    {
+        
+        static Cinema Cnm;
+        static void Main(string[] args)
+        {
+            Application();
+           
+        }
+       
+        static int GetNumber(string message)
+        {
+            bool answer;
+            int num;
+            while (true)
+            {
+                Console.Write(message);
+                answer = int.TryParse(Console.ReadLine(), out num);
+                if (!answer)
+                {
+                    Console.WriteLine("Wrong entry. Please try again.");
+                    continue;
+                }
+                return num;
+            }
+        }
+
+
+
+
+        static void Application() // this method includes the Menu and switch case structure. The program will be executing according to user's input.
+        {
+            Input();
+            Console.WriteLine();
+            Menu();
+            while (true)
+            {
+                Console.WriteLine();
+                string choice = GetChoice();
+                Console.WriteLine();
+                switch (choice)
+                {
+                    case "1":
+                    case "S":
+                        SellTicket(); break;
+                    case "2":
+                    case "R":
+                        RefundTicket(); break;
+                    case "3":
+                    case "D":
+                        StatusInfo(); break;
+                    case "4":
+                    case "X":
+                        Environment.Exit(0); break;
+                }
+            }
+        }
+        static void Input()
+        {
+
+            Console.WriteLine("-------Boutique Cinema-------");
+            Console.WriteLine("");
+            Console.Write("Movie name: ");
+            string movie = Console.ReadLine();
+
+            Console.Write("Capacity: ");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Full Ticket Price: ");
+            float FullTicket = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Half Ticket Price: ");
+            float halfTicket = Convert.ToSingle(Console.ReadLine());
+
+            Cnm = new Cinema(movie, capacity, FullTicket, halfTicket);
+
+        }
+        static void Menu()
+        {
+            Console.WriteLine("1 - Sell Ticket(S)         ");
+            Console.WriteLine("2 - Refund Ticket(R)       ");
+            Console.WriteLine("3 - Status Information(D)  ");
+            Console.WriteLine("4 - Exit(X)                ");
+            Console.WriteLine("                           ");
+
+        }
+        static string GetChoice()
+        {
+            Console.Write("Your Choice: ");
+            return Console.ReadLine().ToUpper();
+        }
+        static void StatusInfo()
+        {
+            Console.WriteLine("Status Information ");
+            Console.WriteLine("Movie: " + Cnm.MovieName);
+            Console.WriteLine("Capacity: " + Cnm.Capacity);
+            Console.WriteLine("Full Ticket Price : " + Cnm.FullTicketPrice);
+            Console.WriteLine("Half Ticket Price : " + Cnm.HalfTicketPrice);
+            Console.WriteLine("Number of Total Full Tickets:  " + Cnm.SumFullTicket);
+            Console.WriteLine("Number of Total Half Tickets: " + Cnm.SumHalfTicket);
+            Console.WriteLine("Turnover: " + Cnm.Turnover);
+            Console.WriteLine("Empty Seat Number: " + Cnm.GetEmptySeat());
+        }
+
+
+        static void SellTicket()
+        {
+            Console.WriteLine("Sell Ticket: ");
+            Console.Write("Number of Full Tickets: ");
+            int full = int.Parse(Console.ReadLine());
+            Console.Write("Number of Half Tickets: ");
+            int half = int.Parse(Console.ReadLine());
+
+            Cnm.SellingTicket(full, half);
+        }
+        static void RefundTicket()
+        {
+            Console.WriteLine("Refund Ticket: ");
+            Console.Write("Number of Full Tickets: ");
+            int full = int.Parse(Console.ReadLine());
+            Console.Write("Number of Half Tickets: ");
+            int half = int.Parse(Console.ReadLine());
+
+            Cnm.RefundTicket(full, half);
+
+         
+        }
+    }
+}
